156605e69a4b68537f1030432ae88f41c07bf0be
KG <kevindeveza.guido@gmail.com>
2021-12-18 09:04:30 +0800

    NA-develop
    - Added Event Class
    - Added initial fetch for users

--- app/src/main/java/com/kdg/hilt/mvvm/ui/users/presentation/MainViewModel.kt
+++ app/src/main/java/com/kdg/hilt/mvvm/ui/users/presentation/MainViewModel.kt
@@ -1,6 +1,5 @@
 package com.kdg.hilt.mvvm.ui.users.presentation
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -8,6 +7,7 @@ import androidx.lifecycle.viewModelScope
 import com.kdg.hilt.mvvm.data.remote.domain.framework.Result
 import com.kdg.hilt.mvvm.data.remote.domain.model.User
 import com.kdg.hilt.mvvm.framework.CoroutinesDispatcherProvider
+import com.kdg.hilt.mvvm.framework.Event
 import com.kdg.hilt.mvvm.ui.users.data.UserRepositoryImpl
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -20,22 +20,29 @@ class MainViewModel @Inject constructor(
     private val userRepositoryImpl: UserRepositoryImpl
 ) : ViewModel() {
 
+    private val _isLoadingIndicatorVisible = MutableLiveData<Boolean>()
+    val isLoadingIndicatorVisible: LiveData<Boolean> = _isLoadingIndicatorVisible
+
+    private val _showError = MutableLiveData<Event<String>>()
+    val showError: LiveData<Event<String>> = _showError
+
     private val _users = MutableLiveData<List<User>>()
     val users: LiveData<List<User>> = _users
 
     fun loadUsers() {
+        _isLoadingIndicatorVisible.value = true
         viewModelScope.launch(dispatcherProvider.computation) {
             val result = userRepositoryImpl.getUsers(0)
             withContext(dispatcherProvider.main) {
                 when (result) {
                     is Result.Success -> {
-                        Log.d("WTF", "${result.data.size}")
-                        Log.d("WTF", "${result.data}")
+                        _users.value = result.data
                     }
                     is Result.Error -> {
-                        Log.d("WTF", "error: ${result.exception}")
+                        _showError.value = Event(result.exception.message ?: "")
                     }
                 }
+                _isLoadingIndicatorVisible.value = false
             }
         }
     }
